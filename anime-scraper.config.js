546f808869b6ea9fea88f09a29da5cfc4af03c0e
アニップ <Hanivan@users.noreply.github.com>
2024-01-05 23:16:06 +0700

    chore: update build script & pm2 config

--- anime-scraper.config.js
+++ anime-scraper.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   apps: [
     {
+      name: 'media-updater',
+      script: 'dist/apps/media-updater/main.js',
+      watch: ['./dist/apps/media-updater'],
+      autorestart: true,
+      cron_restart: '0 * * * *',
+      // interpreter: 'node@16.18.0',
+    },
+    {
       name: 'scraper-service-post',
       script: 'dist/apps/scraper-service/main.js',
       watch: ['./dist/apps/scraper-service'],
