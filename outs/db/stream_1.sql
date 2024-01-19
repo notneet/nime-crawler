ee63dac7c75d0f464bf2be36cdfb9209011a5dd4
アニップ <Hanivan@users.noreply.github.com>
2024-01-19 19:23:20 +0700

    chore: add example row for stream

---
+++ outs/db/stream_1.sql
@@ -0,0 +1,3 @@
+INSERT INTO anime_data.stream_1 (watch_id,author,published,published_ts,name,url,quality,file_size,media_id,created_at,updated_at,num_episode,object_id) VALUES
+	 ('1047',NULL,'2023-09-13 10:12:45','2023-09-13 10:12:45','Kono_Subarashii_Sekai_ni_Bakuen_wo_1.mkv','https://google.com','480',NULL,1,'2023-09-13 10:12:45','2023-09-13 10:12:45',1,'f95c192941084b0646a67e78083dd769');
+
