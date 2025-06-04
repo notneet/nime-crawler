df094f15f7229b7bc1ee4283c31b7131ea390749
アニップ <Hanivan@users.noreply.github.com>
2025-06-04 21:16:12 +0700

    update: mapping microservices
    Signed-off-by: Hanivan Rizky S <hanivan20@gmail.com>

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { ApiGatewayModule } from './api-gateway.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(ApiGatewayModule);
+  await app.listen(process.env.port ?? 3000);
+}
+bootstrap();
